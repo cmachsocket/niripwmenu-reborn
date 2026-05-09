46976d785a3e1b8dcc3bf541f1b31ec2c60dae98
cmachsocket <cmach_socket@outlook.com>
2026-05-09 20:03:55 +0800

    refine cmake resource path

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char* argv[])
     );
 
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/qml/Main.qml"));
+    const QUrl url(QStringLiteral("qrc:///Main.qml"));
 
     QObject::connect(
         &engine, &QQmlApplicationEngine::objectCreated,
